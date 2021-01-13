73b06f00d64e52b910db4366372bc221212c6fe6
aliyussef <a.yussef@outlook.com>
2021-01-14 00:19:28 +0100

    Added Trie

---
+++ src/Node/TrieNode.java
@@ -0,0 +1,14 @@
+package Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrieNode {
+    public Map<Character, TrieNode> children;
+    public boolean endOfWord;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        endOfWord = false;
+    }
+}
